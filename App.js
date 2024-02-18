460f21f68fdaf7a40df7aff13c0cef148e6b1ec0
VikasSuthar101 <160315354+VikasSuthar101@users.noreply.github.com>
2024-02-18 12:33:06 +0530

    Coding Assignment- Artech/ Mckesson Pharamceutical
    Due to lack of time, I could not uploade the whole project .
    
    I've uploaded only Solution spacific files

---
+++ App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Search from './Search';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Bing Search App</h1>
+            <Search />
+        </div>
+    );
+}
+
+export default App;
---
+++ LocationsController.cs
@@ -0,0 +1,31 @@
+﻿using LocationAvailabilityAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocationAvailabilityAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationsController : ControllerBase
+    {
+        private static List<Location> locations = new List<Location>
+    {
+        new Location { Name = "Location 1", AvailabilityStart = TimeSpan.FromHours(10), AvailabilityEnd = TimeSpan.FromHours(13) },
+        new Location { Name = "Location 2", AvailabilityStart = TimeSpan.FromHours(9), AvailabilityEnd = TimeSpan.FromHours(14) }
+        // Add more locations here
+    };
+
+        [HttpGet]
+        public IEnumerable<Location> GetLocationsWithAvailability()
+        {
+            var currentTime = DateTime.Now.TimeOfDay;
+            return locations.FindAll(loc => currentTime >= loc.AvailabilityStart && currentTime <= loc.AvailabilityEnd);
+        }
+
+        [HttpPost]
+        public IActionResult AddLocation(Location location)
+        {
+            locations.Add(location);
+            return Ok();
+        }
+    }
